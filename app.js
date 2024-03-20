664107694ca593aa5f53366e474d389b519284b0
Amtul Sadiya <amtulrahmansadiya.com>
2024-03-20 06:41:41 +0530

    Data types

--- app.js
+++ app.js
@@ -1,12 +1,24 @@
+// Identifiers - let, const & console.log
 
 const Myname = "Sadiya"
 
 let num = 5
 
-console.log(Myname)
+// console.log(Myname)
 
-console.log(num)
+// console.log(num)
 
 num = 10
 
-console.log(num)
+// console.log(num)
+
+
+// Data types
+
+let myNum;
+console.log(myNum)
+let boolvalue = true
+let nullvalue = null
+console.log(boolvalue)
+console.log(nullvalue)
+
